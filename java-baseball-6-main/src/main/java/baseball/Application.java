8f04e261c321751bd3a54d469741519a2d69a2d9
khsqowp <59254269+khsqowp@users.noreply.github.com>
2024-03-10 16:00:59 +0900

    Start

--- java-baseball-6-main/src/main/java/baseball/Application.java
+++ java-baseball-6-main/src/main/java/baseball/Application.java
@@ -1,84 +1,98 @@
 package baseball;
+
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Stream;
-
-import camp.nextstep.edu.missionutils.Randoms;
-import camp.nextstep.edu.missionutils.Console;
-
-
+import java.util.Objects;
 
 
 public class Application {
+
+    static Integer[] youNum;
+
+    //랜덤넘버 만들기
+    static Integer[] youNum2() {
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+        Integer[] youNum2 = new Integer[3];
+        youNum2 = computer.toArray(youNum2);
+
+        youNum = youNum2;
+        return youNum;
+    }
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         boolean restart = false;
         System.out.println("숫자 야구 게임을 시작합니다.");
         do {
-            System.out.print("숫자를 입력해주세요 : ");
+            int strike = 0;
+            int ball = 0;
 
-            //컴퓨터 랜덤 숫자
-            List<Integer> computer = new ArrayList<>();
-            while (computer.size() < 3) {
-                int randomNumber = Randoms.pickNumberInRange(1, 9);
-                if (!computer.contains(randomNumber)) {
-                    computer.add(randomNumber);
-                }
-            }
+            System.out.print("숫자를 입력해주세요 : ");
 
             // 입력 받아오기
             int input_num = Integer.parseInt(Console.readLine());
 
             //세자리 숫자를 배열화
-            int[] you_num = Stream.of(String.valueOf(computer).split("")).mapToInt(Integer::parseInt).toArray();
-            int[] my_num = Stream.of(String.valueOf(input_num).split("")).mapToInt(Integer::parseInt).toArray();
+            Integer[] my_num = new Integer[3];
 
-            //세자리 숫자가 나오는지 검증
-            try {
-                int try_num = input_num;
-                if (1000 > try_num && try_num > 99) {
-                    try_num = 0;
-                }
-            } catch (IllegalArgumentException e) {
-                System.out.println("숫자를 입력해주세요 : ");
+            String num2str = Integer.toString(input_num);
+            String[] str2arr = num2str.split("");
+            for (int i = 0; i < str2arr.length; i++) {
+                my_num[i] = Integer.parseInt(str2arr[i]);
             }
 
-            //스트라이크 볼 판별
-            int strike = 0;
-            int ball = 0;
+            for (int i = 0; i < 2; i++) {
+                int digit = input_num % 10;
+                my_num[2 - i] = digit;
+                input_num /= 10;
+            }
+
             for (int i = 0; i < 3; i++) {
-                if (my_num[i] == (you_num[0] | you_num[2] | you_num[1])) {
-                    ball += 1;
-                }
-            }
-            for (int ii = 0; ii < 3; ii++) {
-                if (my_num[ii] == you_num[ii]) {
+                if (Objects.equals(my_num[i], youNum[i])) {
                     strike += 1;
-                    ball -= 1;
                 }
             }
-            if (ball < 1) {
-                ball = 0;
+
+            for (int i = 0; i < 3; i++) {
+                for (int ii = 0; ii < 2; ii++) {
+                    if (Objects.equals(my_num[i], youNum[ii])) {
+                        ball += 1;
+                    }
+                }
             }
-            if ((my_num[0] != you_num[0]) && (my_num[1] != you_num[1]) && (my_num[2] != you_num[2])) {
+            ball -= strike;
+
+            if (ball == 0 && strike == 0) {
                 System.out.println("낫싱");
-            } else if ((my_num[0] == you_num[0]) && (my_num[1] == you_num[1]) && (my_num[2] == you_num[2])) {
-                System.out.println(strike + "스트라이크");
-                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임종료 \n 게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-                String restartnum = Console.readLine();
-
-                restart = restartnum.equalsIgnoreCase("1");
-
-
-            } else if (ball == 0) {
-                System.out.println(strike + "스트라이크");
             } else {
-                System.out.println(ball + "볼 " + strike + "스트라이크");
+                if (3 > ball && ball > 0) {
+                    System.out.print(ball + "볼");
+                    restart = true;
+                }
+                if (3 > strike && strike > 0) {
+                    System.out.println(strike + "스트라이크");
+                    restart = true;
+                }
+                if (strike == 3) {
+                    System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임종료\n 게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+                    String restartnum = Console.readLine();
+
+                    if (restartnum.equalsIgnoreCase("1")) {
+                        youNum2();
+                    }
+                    restart = restartnum.equalsIgnoreCase("1");
+                }
             }
+
         } while (restart);
-
-
-
 
 
     }
